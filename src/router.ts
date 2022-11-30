fc4330bb48c5ae799edd3818c873adcbcd16f41f
Tainan Nery <zaoldyeck@outlook.com>
2022-11-29 23:50:20 -0300

    feat: router setup

---
+++ src/router.ts
@@ -0,0 +1,41 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/categories', (req, res) => {
+  res.send('ok');
+});
+
+router.post('/categories', (req, res) => {
+  res.send('ok');
+});
+
+router.get('/products', (req, res) => {
+  res.send('ok');
+});
+
+router.post('/products', (req, res) => {
+  res.send('ok');
+});
+
+router.get('/categories/:categoryId/products', (req, res) => {
+  res.send('ok');
+});
+
+router.get('/orders', (req, res) => {
+  res.send('ok');
+});
+
+router.post('/orders', (req, res) => {
+  res.send('ok');
+});
+
+router.patch('/orders/:orderId', (req, res) => {
+  res.send('ok');
+});
+
+router.delete('/orders/:orderId', (req, res) => {
+  res.send('ok');
+});
+
+export { router };
